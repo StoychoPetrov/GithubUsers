35392551ce2db89b4b3ed1d8e67307e06f9d4ae1
StoychoPetrov <stoychopetrov95@gmail.com>
2019-01-26 09:56:21 +0200

    Code refactor

--- app/src/main/java/com/example/githubusers/RestfulAPi.kt
+++ app/src/main/java/com/example/githubusers/RestfulAPi.kt
@@ -5,14 +5,20 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
 
+// QUERY STRINGS
+const val QUERY_USERS = "since";
+
+//PATHS
+const val PATH_LOGIN = "login"
+
 interface RestfulAPi {
 
     // Http "GET" request to url with path "/users" with query string: "?since=", the expected result is array with users
     @GET("users")
-    fun getGitHubUsers(@Query("since") since: String) : Observable<ArrayList<UserModel>>
+    fun getGitHubUsers(@Query(QUERY_USERS) since: String) : Observable<ArrayList<UserModel>>
 
     // Http "GET" request to url with path "/users/{login}" and user's login like param
     @GET("users/{login}")
-    fun getGitHubUserDetails(@Path("login") login: String?) : Observable<UserModel>
+    fun getGitHubUserDetails(@Path(PATH_LOGIN) login: String?) : Observable<UserModel>
 
 }
--- app/src/main/java/com/example/githubusers/Utils.kt
+++ app/src/main/java/com/example/githubusers/Utils.kt
@@ -5,11 +5,11 @@ import java.net.IDN
 object Utils {
 
     // RESTFUL API URLS
-    const val GITHUB_GET_USERS_URL = "https://api.github.com/";
+    const val GITHUB_GET_USERS_URL                  = "https://api.github.com/";
 
     // INTENT EXTRAS
-    const val INTENT_USER          = "user";
+    const val INTENT_USER                           = "user";
 
     // LOG TAGS
-    const val TAG_HTTP_CONNECTION_FAILED_MESSAGE = "CONNECION_MESSAGE"          // TAG for logging connection problem with remote restFul API
+    const val TAG_HTTP_CONNECTION_FAILED_MESSAGE    = "CONNECION_MESSAGE"          // TAG for logging connection problem with remote restFul API
 }
